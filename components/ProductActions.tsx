811b2c18c2bf47acc82ba73af633a6fb2852b57d
kyzavlad <kuzya.universe@gmail.com>
2026-08-19 17:25:17 +0300

    copy: use universal wholesale packaging label

--- components/ProductActions.tsx
+++ components/ProductActions.tsx
@@ -1,4 +1,4 @@
 'use client'
 import type { Product } from '@/lib/types'
 import { useStore } from './StoreProvider'
-export function ProductActions({product}:{product:Product}){const {add}=useStore();return <button className="primary full" onClick={()=>add(product)}>Добавить 1 ящик в корзину</button>}
+export function ProductActions({product}:{product:Product}){const {add}=useStore();return <button className="primary full" onClick={()=>add(product)}>Добавить 1 упаковку в корзину</button>}
